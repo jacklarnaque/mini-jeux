f56074e8fd47852b89918c17d6c32669c4a647ca
jacklarnaque <grousset1065@live.fr>
2022-11-01 00:49:55 +0100

    working on victory condition

--- app.js
+++ app.js
@@ -16,12 +16,15 @@ function getRandomIntInclusive(min, max) {
 
 let randomNumber = getRandomIntInclusive(1, 3);
 let randomNumber2 = getRandomIntInclusive(4, 7);
+let randomNumber3 = getRandomIntInclusive(randomNumber, randomNumber2);
+ 
 
 // we use the button 'play' to run the function
 playButton.addEventListener('click', start); 
 
 function start () {
-    if (randomNumber < randomNumber2) {
+    let answer = document.getElementById("input").value;
+    if (answer < randomNumber2) {
         document.getElementById('numbersRange').textContent = `Veuillez devinez le prix de cet aricle..Sa valeur est comprise entre ${randomNumber}
         et ${randomNumber2}.`;
     } else if (randomNumber > randomNumber2) {
@@ -34,14 +37,15 @@ function start () {
 // bug si l'ont met un numéros qui n'est pas dans le range logger
 function getValueUser () {
     let answer = document.getElementById("input").value;
-    if(answer < randomNumber){
-        console.log("Ce nombre n'est pas compris entre " + randomNumber + " et " +randomNumber2 + '.' +
-        ' Veuillez écrire un chiffre plus grand que ' + randomNumber);
-    } else if(answer > randomNumber2) {
-        console.log("Ce nombre n'est pas compris entre " + randomNumber + " et " +randomNumber2 + '.' +
-        ' Veuillez écrire un chiffre plus petit que ' + randomNumber2);
-    } else {
-        console.log("C'est gagner!")
+    if(answer < randomNumber && answer > randomNumber2){
+        console.error("Ce nombre n'est pas compris entre " + randomNumber + " et " +randomNumber2 + '.');
+    // } else if(answer > randomNumber2) {
+    //     console.error("Ce nombre n'est pas compris entre " + randomNumber + " et " +randomNumber2 + '.' +
+    //     ' Veuillez écrire un chiffre plus petit que ' + randomNumber2);
+    } else if(answer > randomNumber3){
+        document.getElementById('numbersRange').textContent = answer + "Too high! ";
+    } else if(answer < randomNumber3){
+        document.getElementById('numbersRange').textContent = answer + "Too low! ";
     }
 }
 
@@ -49,3 +53,7 @@ function getValueUser () {
 // to do :
 // problem to define the display of the div 'in'(l15 html). I can change any style and i cant see
 // the style in the dev tools...
+
+//!!!<----------PRIORITY!!!--------->
+// I need to create a random number which the value have to be between randomNumber et randomNumber2
+//!!!<----------PRIORITY!!!--------->
